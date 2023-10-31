3a5edff1990c191697c0a328db1f5647dcb4d764
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-10-31 12:10:00 +0300

    Singlton realisation

--- day_01/ex01/Program.java
+++ day_01/ex01/Program.java
@@ -13,6 +13,28 @@ class Program {
         System.out.println("first      " + first.toString());
         System.out.println("second     " + second.toString());
 
+        System.out.println();
+        System.out.println("Создаем несколько новых пользователей и проверяем уникальность " +
+                "id у каждого из них");
+        System.out.println("---------------------------------------------------------------" +
+                "-------------------");
+
+        User person1 = new User("Анна Иванова", 5555);
+        User person2 = new User("Иван Петров", 55678);
+        User person3 = new User("Алексей Смирнов", 14325);
+        User person4 = new User("Дмитрий Тихонов", 6987);
+
+        System.out.printf("%-10s%-20s%-5s%d\n",  "person1", person1.getName(), "ID - ",
+                person1.getId());
+        System.out.printf("%-10s%-20s%-5s%d\n",  "person2", person2.getName(), "ID - ",
+                person2.getId());
+        System.out.printf("%-10s%-20s%-5s%d\n",  "person3", person3.getName(), "ID - ",
+                person3.getId());
+        System.out.printf("%-10s%-20s%-5s%d\n",  "person4", person4.getName(), "ID - ",
+                person4.getId());
+
+
+
 
 
     }
