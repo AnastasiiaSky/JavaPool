d083e83cce399d751b368810f2440077682985f4
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-10-31 11:58:46 +0300

    Singlton realisation

--- day_01/ex01/Program.java
+++ day_01/ex01/Program.java
@@ -2,5 +2,18 @@ package ex01;
 
 class Program {
     public static void main(String[] args) {
+        UserIdsGenerator first = UserIdsGenerator.getInstance();
+        UserIdsGenerator second = UserIdsGenerator.getInstance();
+
+        System.out.println("Подтверждение, что генератор ID создается только 1 раз.");
+        System.out.println("При выводе текстового представления объекта мы видим что две " +
+                "ссылочные переменные ссылаются на один объект в памяти");
+        System.out.println("--------------------------------------------------------------" +
+                "------------------------------------------------------");
+        System.out.println("first      " + first.toString());
+        System.out.println("second     " + second.toString());
+
+
+
     }
 }
--- day_01/ex01/UserIdsGenerator.java
+++ day_01/ex01/UserIdsGenerator.java
@@ -1,5 +1,7 @@
 package ex01;
 
+// Singlton - дает гарантию что у класса будет всего один экземпляр класса.
+// Предоставляет глобальную точку доступа к экземпляру данного класса
 public final class UserIdsGenerator {
 
     private static final UserIdsGenerator INSTANCE = new UserIdsGenerator();
