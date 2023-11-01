ee512c4d6196f421eeedf53bd5f138d05dd96adb
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-11-01 17:09:27 +0300

    2 redo

--- day_01/ex02/Program.java
+++ day_01/ex02/Program.java
@@ -6,30 +6,29 @@ class Program {
         User person2 = new User("Иван Петров", 55678);
         User person3 = new User("Алексей Смирнов", 14325);
         User person4 = new User("Дмитрий Тихонов", 6987);
-        User person5 = new User("Юлия Кулакова", 3333);
-        User person6 = new User("Иван Злобин", 4444);
-        User person7 = new User("Максим Чкалов", 6666);
-        User person8 = new User("Дмитрий Менделеев", 7777);
-        User person9 = new User("Борис Акунин", 8888);
-        User person10 = new User("Николай Островаский", 9999);
-        User person11 = new User("Владимир Путин", 10000);
+        User person5 = new User("Владимир Путин", 6987);
+        User person6 = new User("Дмитрий Медведев", 6987);
+        User person7 = new User("Александр Блок", 6987);
+        User person8 = new User("Федор Достоевский", 6987);
+        User person9 = new User("Сергей Есенин", 6987);
+        User person10 = new User("Борис Акунин", 6987);
+        User person11 = new User("Антон Чехов", 6987);
+        User person12 = new User("Александр Пушкин", 6987);
+        User person13 = new User("Сергей Минаев", 6987);
+        User person14 = new User("Андрей Рублев", 6987);
 
         UsersArrayList testList = new UsersArrayList();
         System.out.println("UsersArrayList после создания.");
-        System.out.println("------------------------------");
-
-        System.out.printf("%-23s%d\n%-23s%d\n",  "Список пользователей - ", testList.getUsersCount(), "Размер массива - ",
-                testList.getCapacity());
-        System.out.println();
+        printInfoAboutUsersList(testList);
 
         testList.addUser(person1);
-        System.out.println("Добавляем нового пользователя в UsersArrayList");
-        System.out.println("----------------------------------------------");
-        System.out.printf("%-23s%d\n%-23s%d\n%-23s%s ID %d\n",  "Список пользователей - ", testList.getUsersCount(),
-                "Размер массива - ", testList.getCapacity(), "Добавленный пользователь - ",
-                testList.getUserByIndex(0).getName(), testList.getUserByIndex(0).getId());
         testList.addUser(person2);
         testList.addUser(person3);
+
+        System.out.println("Добавляем новых пользователей в UsersArrayList");
+        printInfoAboutUsersList(testList);
+
+        System.out.println("Добавляем больше 10 новых пользователей в UsersArrayList");
         testList.addUser(person4);
         testList.addUser(person5);
         testList.addUser(person6);
@@ -38,12 +37,12 @@ class Program {
         testList.addUser(person9);
         testList.addUser(person10);
         testList.addUser(person11);
-        System.out.println();
-        System.out.println("Добавляем еще 10 пользователей в UsersArrayList");
-        System.out.println("-----------------------------------------------");
-        System.out.printf("%-23s%d\n%-23s%d\n",  "Список пользователей - ", testList.getUsersCount(), "Размер массива - ",
-                testList.getCapacity());
-        System.out.println();
+        testList.addUser(person12);
+        testList.addUser(person13);
+        testList.addUser(person14);
+        printInfoAboutUsersList(testList);
+
+
         System.out.println("Получаем пользователя по индексу 3 и по индексу 10");
         System.out.println("--------------------------------------------------");
         System.out.printf("%-23s%s ID %d\n",  "UsersArrayList[3] - ",
@@ -56,8 +55,12 @@ class Program {
         System.out.printf("%-23s%s ID %d\n",  "UsersArrayList[3] - ",
                 testList.getUserById(4).getName(), testList.getUserById(4).getId());
         System.out.printf("%-23s%s ID %d\n",  "UsersArrayList[11] - ",
-                testList.getUserById(11).getName(), testList.getUserById(11).getId());
+                testList.getUserById(11).getName(), testList.getUserById(11).getId());4
+    }
 
 
+    public static void printInfoAboutUsersList(UsersArrayList testList) {
+        System.out.printf("%-23s%d\n%-23s%d\n",  "Колличество пользователей - ", testList.getUsersCount(),
+                "Размер массива - ", testList.getCapacity());
     }
 }
