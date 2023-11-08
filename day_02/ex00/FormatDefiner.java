5f50012dd8176a05b3f2e415719e0430897916a8
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-11-08 15:15:28 +0300

    ex00 done

--- day_02/ex00/FormatDefiner.java
+++ day_02/ex00/FormatDefiner.java
@@ -15,105 +15,108 @@ import java.util.Scanner;
 
 class FormatDefiner {
     int sizeC = -1;
-   private List<String> formats;
-   Map<String, List<String>> formatPairs;
+    private List<String> formats;
+    Map<List<String>, String> formatPairs;
 
 
-   public FormatDefiner() {
-       this.formats = getFormatsFromFile("./ex00/signal.txt");
-       if(this.formats == null) throw new RuntimeException("Некорректные данные в файле signal.txt");
-       this.formatPairs = parseSignatures(formats);
+    public FormatDefiner() {
+        this.formats = getFormatsFromFile("./ex00/signal.txt");
+        if (this.formats == null) throw new RuntimeException("Некорректные данные в файле signal.txt");
+        this.formatPairs = parseSignatures(formats);
 
-   }
+    }
 
-   private List<String> getFormatsFromFile(String path) {
-       List<String> fileData = new ArrayList<String>();
-       try {
-           InputStream fin = new FileInputStream(path);
-           StringBuilder inputData = new StringBuilder();
-           while(fin.available() > 0) {
-               inputData.append((char)fin.read());
-           }
-           StringBuilder curLine = new StringBuilder();
-           for(int it = 0; it < inputData.length(); ++it) {
-               if(inputData.charAt(it) == '\n') {
-                   fileData.add(curLine.toString());
-                   curLine = new StringBuilder();
-               } else curLine.append(inputData.charAt(it));
-           }
-           fin.close();
-       } catch(IOException e) {
-           e.printStackTrace();
-       }
-       return fileData;
-   }
+    private List<String> getFormatsFromFile(String path) {
+        List<String> fileData = new ArrayList<String>();
+        try {
+            InputStream fin = new FileInputStream(path);
+            StringBuilder inputData = new StringBuilder();
+            while (fin.available() > 0) {
+                inputData.append((char) fin.read());
+            }
 
-   private Map<String, List<String>> parseSignatures(List<String> formats)  {
-       Map<String, List<String>> formatPairs = new HashMap<>();
-       for(String element : formats) {
-           String[] data = element.split(", ");
-           String[] signals = data[1].split(" ");
-           List<String> byteList = new ArrayList<>();
-           for(String part : signals) {
-               byteList.add(part);
-           }
-           formatPairs.put(data[0], byteList);
+            StringBuilder curLine = new StringBuilder();
+            for (int it = 0; it < inputData.length(); ++it) {
+                if (inputData.charAt(it) == '\n') {
+                    fileData.add(curLine.toString());
+                    curLine = new StringBuilder();
+                } else curLine.append(inputData.charAt(it));
+            }
+            fin.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return fileData;
+    }
 
-               System.out.println(formatPairs.toString());
-       }
-       return formatPairs;
-   }
+    private Map<List<String>, String> parseSignatures(List<String> formats) {
+        Map<List<String>, String> formatPairs = new HashMap<>();
+        for (String element : formats) {
+            String[] data = element.split(", ");
+            String[] signals = data[1].split(" ");
+            List<String> byteList = new ArrayList<>();
+            for (String part : signals) {
+                byteList.add(part);
+            }
+            formatPairs.put(byteList, data[0]);
+        }
+        return formatPairs;
+    }
 
-//
-//               for (Map.Entry<String, String> e : signature.entrySet()) {
-//        if (e.getValue().equals(sb.substring(0, e.getValue().length()))) {
-//            res.add(e.getKey());
-//            System.out.println("PROCESSED");
-//            break;
-//        }
-//    }
-//
-//            if (size == res.size()) {
-//        res.clear();
-//        break;
-//    }
-//
-//    line = sc.nextLine();
-//            in.close();
-//}
-//        sc.close();
-//                return res;
-//                }
     public void execute() {
-       Scanner sc = new Scanner(System.in);
-        List<String> results = new LinkedList<>();
-        while(true) {
+        Scanner sc = new Scanner(System.in);
+        List<String> results = new ArrayList<>();
+        while (true) {
             System.out.println("Введите путь к файлу или введите 42 для выхода");
             System.out.println("->");
-            String path = sc.nextLine();
-            if(path.equals("42")) System.exit(0);
+            String path = sc.nextLine().trim();
+            if (path.equals("42")) System.exit(0);
             try {
-                InputStream inputFile = new FileInputStream(path));
+                InputStream inputFile = new FileInputStream(path);
                 StringBuilder str = new StringBuilder();
                 for (int i = 0; i < 3; i++) {
-                    sb.append(String.format("%02X", in.read()));
+                    str.append(String.format("%02X", inputFile.read()));
+                    if (i != 2) str.append(",");
                 }
-
-
-            } catch (IOException e){
-                System.out.println(e.getMessage());
+                String[] firstBytes = str.toString().split(",");
+                String answer = getFormatFromMap(firstBytes);
+                if(answer == null) {
+                    System.out.println("UNDEFINED");
+                    results = new ArrayList<>();
+                } else {
+                    System.out.println("PROCESSED");
+                    results.add(answer);
+                }
+                writeToFile(results);
+            } catch (IOException e) {
+                System.out.println(e.getMessage() + ": Файл не найден!");
             }
 
         }
-//        for(String s : formats) {
-//            System.out.println(s);
-//        }
     }
 
-//    public void execute() {
-//
-//       Scanner fsc = new Scanner(System.in);
-//        System.out.println("Введите путь к проверяемому файлу");
-//        System.out.print("->");
-//    }
+    public String getFormatFromMap(String[] firstBytes) {
+        for (Map.Entry<List<String>, String> element : formatPairs.entrySet()) {
+            List<String> bytes = element.getKey();
+            for (int i = 0; i < bytes.size(); ++i) {
+                if (bytes.get(i).equals(firstBytes[i]) && bytes.get(i + 1).equals(firstBytes[i + 1])
+                        && bytes.get(i + 2).equals(firstBytes[i + 2])) {
+                        return element.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+    public void writeToFile(List<String> results) {
+        try {
+            OutputStream fos = new FileOutputStream("ex00/result.txt", true);
+            for (int i = 0; i < results.size(); ++i) {
+                fos.write(results.get(i).getBytes());
+                fos.write("\n".getBytes());
+            }
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
--- day_02/ex00/SignalFormatException.java
+++
@@ -1,9 +0,0 @@
-package ex00;
-
-public class SignalFormatException extends RuntimeException {
-
-    @Override
-    public String toString() {
-        return ("IlligalSignalFormatException");
-    }
-}
