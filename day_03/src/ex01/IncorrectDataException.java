039f38afd0ad79d589688883946e884b42e23683
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-11-27 12:05:59 +0300

    ex01 works

---
+++ day_03/src/ex01/IncorrectDataException.java
@@ -0,0 +1,8 @@
+package ex01;
+
+public class IncorrectDataException extends RuntimeException{
+    @Override
+    public String toString() {
+        return ("IncorrectDataException");
+    }
+}
---
+++ day_03/src/ex01/ProducerConsumer.java
@@ -0,0 +1,32 @@
+package ex01;
+
+import java.util.Stack;
+
+public class ProducerConsumer {
+
+    private Stack<String> data;
+    private Object muter;
+
+    public ProducerConsumer(Object muter, Stack<String> data) {
+        this.muter = muter;
+        this.data = data;
+    }
+
+
+    public void putObj(String object) throws InterruptedException {
+        synchronized (muter) {
+            if (object.equals("Egg")) {
+                while (data.size() % 2 != 0) {
+                    muter.wait();
+                }
+            } else {
+                while (data.size() % 2 == 0) {
+                    muter.wait();
+                }
+            }
+            data.push(object);
+            System.out.println(object);
+            muter.notifyAll();
+        }
+    }
+}
