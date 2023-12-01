9a674035ce04f1623af2526e9324c1006126ea7b
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-12-01 15:45:58 +0300

    ex00 completed

---
+++ day_04/ex00/ImagesToChar/src/java/edu/school21/printer/app/Program.java
@@ -0,0 +1,21 @@
+package ImagesToChar.src.java.edu.school21.printer.app;
+
+import ImagesToChar.src.java.edu.school21.printer.logic.Converter;
+
+public class Program {
+    public static void main(String[] args) {
+        if(args.length == 3 && args[0].length() == 1 && args[1].length() == 1) {
+            char[] white = args[0].trim().toCharArray();
+            char[] black = args[1].trim().toCharArray();
+            String path = args[2].trim();
+            new Converter(white[0], black[0], path);
+        } else {
+            System.out.println("В качестве аргументов программы должны поступать два символа," +
+                    " введенные через пробел и путь к файлу");
+        }
+    }
+}
+
+
+
+
