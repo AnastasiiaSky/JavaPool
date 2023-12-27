4bf54265b9c785c6fb96a463b7d7fb9b3221a07d
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-12-27 19:34:42 +0300

    ex00

---
+++ day_06/ex00/Tests/src/main/java/edu.school21.numbers/NumberWorker.java
@@ -0,0 +1,22 @@
+package edu.school21.numbers;
+
+public class NumberWorker {
+    public static boolean isPrime(final int number) throws IllegalNumberException {
+        if (number < 2) {
+            throw new IllegalNumberException("The number should be >= 2!");
+        }
+        for (int it = 2; it <= Math.sqrt(number); ++it) {
+            if (number % it == 0) return false;
+        }
+        return true;
+    }
+
+    public static int digitsSum(int number) {
+        int result = 0;
+        do {
+            result += number % 10;
+            number /= 10;
+        } while (number != 0);
+        return result;
+    }
+}
