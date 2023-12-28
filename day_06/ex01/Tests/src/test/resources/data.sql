525d96bbbbcbb11709391c41e71f98c5852fd15b
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2023-12-28 17:27:06 +0300

    ex01 done

---
+++ day_06/ex01/Tests/src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Product(id, name, price) VALUES(1, 'milk', 89.90);
+INSERT INTO Product(id, name, price) VALUES(2, 'bread', 48.90);
+INSERT INTO Product(id, name, price) VALUES(3, 'chocolate', 110.90);
+INSERT INTO Product(id, name, price) VALUES(4, 'cookie', 156.90);
+INSERT INTO Product(id, name, price) VALUES(5, 'rice', 75.90);
---
+++ day_06/ex01/Tests/src/test/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS Product;
+
+
+CREATE TABLE IF NOT EXISTS Product (
+    id                  INTEGER PRIMARY KEY NOT NULL,
+    name                VARCHAR(250)        NOT NULL,
+    price               FLOAT               NOT NULL
+);
+
