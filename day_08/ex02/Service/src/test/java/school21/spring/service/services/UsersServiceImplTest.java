71c77fb4d1aab9513d3c0e74722dc649141e840f
Qyle Nettie <qylenett@at-b2.msk.21-school.ru>
2024-01-30 18:02:08 +0300

    ex02 done

--- day_08/ex02/Service/src/test/java/school21/spring/service/services/UsersServiceImplTest.java
+++ day_08/ex02/Service/src/test/java/school21/spring/service/services/UsersServiceImplTest.java
@@ -1,4 +1,39 @@
 package school21.spring.service.services;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import school21.spring.service.config.TestApplicationConfig;
+import school21.spring.service.repositories.UsersRepository;
+import school21.spring.service.repositories.UsersRepositoryJdbcTemplateImpl;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UsersServiceImplTest {
+
+    TestApplicationConfig testApplicationConfig;
+    private DataSource dataSource;
+    private UsersRepository usersRepository;
+    private UsersServiceImpl usersService;
+
+    @BeforeEach
+    public void init() {
+        testApplicationConfig = new TestApplicationConfig();
+        testApplicationConfig.init();
+        dataSource = testApplicationConfig.db;
+        usersRepository = new UsersRepositoryJdbcTemplateImpl(dataSource);
+        usersService = new UsersServiceImpl(usersRepository);
+    }
+
+    @ParameterizedTest
+    @DisplayName("sing_up_with_values_returns_right_result_jdbc_template")
+    @ValueSource(strings = {"qylenett@yandex.com", "sparrvio@yandex.com", "morfinpo@yandex.com", "jettajac@yandex.com", "delilahl@yandex.com"})
+    void sing_up_with_values_returns_right_result_jdbc_template(String email) {
+        String password = usersService.signUp(email);
+        assertNotNull(password);
+    }
 }
--- day_08/ex02/Service/target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists "Users";
-
-create table "Users" (
-    id bigserial primary key,
-    email text unique not null,
-    password text not null
-)
